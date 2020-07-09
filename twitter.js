5ffd0df18803c718f5c438d4498e3d835393b61c
Lane Herberholz <lherberholz@odneycomm.com>
2020-07-09 16:33:06 -0500

    working on batch

--- twitter.js
+++ twitter.js
@@ -1,7 +1,6 @@
 const twitter = require("twitter");
-const fs = require("fs");
 
-const initialize = async (username) => {
+const getTweetBatch = async (twitParams) => {
     const twitClient = new twitter({
         consumer_key: "JQjtyssiKnTzFkMA79S90t4PU",
         consumer_secret: "sRRrjSFJr5MMEW7LWCBD8M65DwUqfQZeei6VjZH5UmGaLQb2FI",
@@ -9,6 +8,14 @@ const initialize = async (username) => {
         access_token_secret: "nJrUnJJHL1C2MfcU8XOvE4qVlEsROzd9Zlv9qBZd9XuDD",
     });
 
+    const tweetText = [];
+
+    twitClient.get("statuses/user_timeline", twitParams).then((tweets, error, res) => {
+        return tweets;
+    });
+};
+
+const initialize = async (username) => {
     const twitParams = {
         screen_name: username,
         exclude_replies: false,
@@ -17,26 +24,34 @@ const initialize = async (username) => {
         count: 200
     };
 
+    const twitClient = new twitter({
+        consumer_key: "JQjtyssiKnTzFkMA79S90t4PU",
+        consumer_secret: "sRRrjSFJr5MMEW7LWCBD8M65DwUqfQZeei6VjZH5UmGaLQb2FI",
+        access_token_key: "421317769-LWaNcsTwEt0StS3sh8qUwsR3E2sx8wth8DMpn9QK",
+        access_token_secret: "nJrUnJJHL1C2MfcU8XOvE4qVlEsROzd9Zlv9qBZd9XuDD",
+    });
+
     const END_OF_TWEETS = false;
     const allTweetsText = [];
 
     
+    const temp = await getTweetBatch(twitParams);
+    console.log(temp.length);
 
     // twitClient.get("statuses/user_timeline", twitParams).then((tweets) => {
-    //     console.log(tweets.length);
+    //     console.log(tweets.text);
     // });
 
-    while (!END_OF_TWEETS || allTweetsText.length <= 400) {
-        twitClient.get("statuses/user_timeline", twitParams).then((tweets, error, res) => {
-            if (!error) {
-                for (tweet of tweets) {
-                    allTweetsText.push(tweet.text);
-                }
-                twitParams.max_id=tweets[tweets.length-1];
-                console.log(allTweetsText);
-            } else { END_OF_TWEETS = true; }
-        });
-    }
+    // while (!END_OF_TWEETS || allTweetsText.length <= 400) {
+    //     const batchReturn = await getTweetBatch(twitParams);
+    //     console.log(batchReturn);
+    //     // if (batchReturn==false) {
+    //     //     END_OF_TWEETS=true;
+    //     // } else {
+    //     //     allTweetsText.push(...batchReturn[0]);
+    //     //     twitParams.max_id = batchReturn[1];
+    //     // }
+    // }
 };
 
 exports.initialize = initialize;
