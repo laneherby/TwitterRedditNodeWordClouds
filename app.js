77d9463d59a489e8b6d8bdef8cf26cec005fb1e3
Lane Herberholz <lherberholz@odneycomm.com>
2020-07-13 15:33:40 -0500

    Worked on folder structure and started express and ejs

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const index = require("./routes/index");
+const app = express();
+const port = 3000;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// set path for static assets
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// routes
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error', {status:err.status, message:err.message});
+});
+
+app.listen(port)
+
+// reddit.initialize("node", "top", 50);
+
+// twitter.initialize("laneherby", true, 25);
--- index.js
+++
@@ -1,6 +0,0 @@
-const reddit = require("./reddit");
-const twitter = require("./twitter");
-
-//reddit.initialize("node", "top", 50);
-
-twitter.initialize("laneherby", true, 25);
