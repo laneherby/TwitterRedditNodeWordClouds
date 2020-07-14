99e862b2cf989ef26b4f590d4d68545c2f2cb376
Lane Herberholz <lherberholz@odneycomm.com>
2020-07-14 15:15:46 -0500

    CSS and html

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // set path for static assets
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, '/public')));
 
 // routes
 app.use('/', index);
