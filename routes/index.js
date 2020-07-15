a74455ae0dd3ec651917e3bf5db2aaecad5a0d14
Lane Herberholz <lherberholz@odneycomm.com>
2020-07-15 16:17:59 -0500

    Working on creating pages for inputs

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,12 @@ router.get('/', (req, res) => {
     res.render("index", {page: "Home"});
 });
 
+router.get('/twitter', (req, res) => {
+    res.render("twitter", {page: "Twitter Cloud"});
+});
+
+router.get('/reddit', (req, res) => {
+    res.render("reddit", {page: "Reddit Cloud"});
+});
+
 module.exports = router;
